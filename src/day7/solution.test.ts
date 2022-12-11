04bf061870025209bc13769aba46576c14bb4b51
Klee Thomas <klee.ut@gmail.com>
2022-12-11 11:14:53 +1100

    :construction: part 1 sample passes but input fails

---
+++ src/day7/solution.test.ts
@@ -0,0 +1,117 @@
+import { describe, expect, it, test } from "vitest";
+import {
+  DirectoryFileSystemNode,
+  FileFileSystemNode,
+  part1,
+  readToTree,
+  sumForDirectory,
+} from "./solution";
+describe("day template", () => {
+  it("should read lines to file system", () => {
+    const outputLines = ["$ cd /"];
+    expect(readToTree(outputLines)).toEqual(new DirectoryFileSystemNode("/"));
+  });
+  it("should read lines to file system one level", () => {
+    const outputLines = ["$ cd /", "$ ls", "14848514 b.txt"];
+    const root = new DirectoryFileSystemNode("/");
+    root.addFileChildNode(new FileFileSystemNode("b.txt", 14848514, root));
+    expect(readToTree(outputLines)).toEqual(root);
+  });
+  it("should read lines to file system with directory", () => {
+    const outputLines = [
+      "$ cd /",
+      "$ ls",
+      "dir a",
+      "$ cd a",
+      "$ ls",
+      "14848514 b.txt",
+    ];
+    const root = new DirectoryFileSystemNode("/");
+    const a = new DirectoryFileSystemNode("a", root);
+    root.addDirectoryChildNode(a);
+    a.addFileChildNode(new FileFileSystemNode("b.txt", 14848514, a));
+    expect(readToTree(outputLines)).toEqual(root);
+  });
+
+  it("should read lines to file system with multiple directories", () => {
+    const outputLines = [
+      "$ cd /",
+      "$ ls",
+      "dir a",
+      "dir b",
+      "$ cd a",
+      "$ ls",
+      "14848514 c.txt",
+      "$ cd ..",
+      "$ cd b",
+      "14848514 d",
+    ];
+    const root = new DirectoryFileSystemNode("/");
+    const a = new DirectoryFileSystemNode("a", root);
+    root.addDirectoryChildNode(a);
+    a.addFileChildNode(new FileFileSystemNode("c.txt", 14848514, a));
+    const b = new DirectoryFileSystemNode("b", root);
+    root.addDirectoryChildNode(b);
+    b.addFileChildNode(new FileFileSystemNode("d", 14848514, b));
+    expect(readToTree(outputLines)).toEqual(root);
+  });
+  it("should read lines to file system with multiple directories and mixed content", () => {
+    const outputLines = [
+      "$ cd /",
+      "$ ls",
+      "1234 z.json",
+      "dir a",
+      "dir b",
+      "$ cd a",
+      "$ ls",
+      "14848514 c.txt",
+      "$ cd ..",
+      "$ cd b",
+      "14848514 d",
+    ];
+    const root = new DirectoryFileSystemNode("/");
+    root.addFileChildNode(new FileFileSystemNode("z.json", 1234, root));
+    const a = new DirectoryFileSystemNode("a", root);
+    root.addDirectoryChildNode(a);
+    a.addFileChildNode(new FileFileSystemNode("c.txt", 14848514, a));
+    const b = new DirectoryFileSystemNode("b", root);
+    root.addDirectoryChildNode(b);
+    b.addFileChildNode(new FileFileSystemNode("d", 14848514, b));
+    expect(readToTree(outputLines)).toEqual(root);
+  });
+  describe("sum size by directory", () => {
+    it("simple directory", () => {
+      const root = new DirectoryFileSystemNode("/");
+      root.addFileChildNode(new FileFileSystemNode("d", 84, root));
+
+      expect(sumForDirectory(root)).toEqual([{ name: "/", size: 84 }]);
+    });
+    it("nested directory", () => {
+      const root = new DirectoryFileSystemNode("/");
+      root.addFileChildNode(new FileFileSystemNode("a", 84, root));
+      const b = new DirectoryFileSystemNode("b", root);
+      root.addDirectoryChildNode(b);
+      b.addFileChildNode(new FileFileSystemNode("c", 1900, root));
+
+      expect(sumForDirectory(root)).toEqual([
+        { name: "/", size: 1984 },
+        { name: "b", size: 1900 },
+      ]);
+    });
+  });
+  describe("part 1", () => {
+    test("full test", () => {
+      const outputLines = [
+        "$ cd /",
+        "$ ls",
+        "dir a",
+        "200 b.txt",
+        "$ cd a",
+        "$ ls",
+        "99999 c.txt",
+      ];
+
+      expect(part1(outputLines.join("\n"))).toEqual(99999);
+    });
+  });
+});
---
+++ src/day7/solution.ts
@@ -0,0 +1,139 @@
+const oneHundredThousand = 100000;
+export enum FileSystemNodeType {
+  file,
+  directory,
+}
+export interface FileSystemNode {
+  name: string;
+  type: FileSystemNodeType;
+  parent?: FileSystemNode;
+}
+
+export class DirectoryFileSystemNode implements FileSystemNode {
+  readonly type: FileSystemNodeType.directory;
+  readonly name: string;
+  private _fileChildren: Map<string, FileFileSystemNode>;
+  private _directoryChildren: Map<string, DirectoryFileSystemNode>;
+  constructor(name: string, public readonly parent?: DirectoryFileSystemNode) {
+    this.type = FileSystemNodeType.directory;
+    this.name = name;
+    this._fileChildren = new Map();
+    this._directoryChildren = new Map();
+  }
+  addFileChildNode(node: FileFileSystemNode): void {
+    if (this._fileChildren.has(node.name)) {
+      console.log("Overwriting file", node.name, {
+        old: this._fileChildren.get(node.name)?.size,
+        new: node.size,
+      });
+    }
+    this._fileChildren.set(node.name, node);
+  }
+  addDirectoryChildNode(node: DirectoryFileSystemNode): void {
+    if (this._directoryChildren.has(node.name)) {
+      console.log("Overwriting directory", node.name);
+    }
+    this._directoryChildren.set(node.name, node);
+  }
+  get fileChildren(): FileFileSystemNode[] {
+    return Array.from(this._fileChildren.values());
+  }
+  get directoryChildren(): DirectoryFileSystemNode[] {
+    return Array.from(this._directoryChildren.values());
+  }
+}
+export class FileFileSystemNode implements FileSystemNode {
+  readonly type: FileSystemNodeType.file;
+  readonly name: string;
+  readonly size: number;
+  constructor(
+    name: string,
+    size: number,
+    public readonly parent: DirectoryFileSystemNode
+  ) {
+    this.type = FileSystemNodeType.file;
+    this.name = name;
+    this.size = size;
+  }
+}
+
+export function readToTree(outputLines: string[]): DirectoryFileSystemNode {
+  const root = new DirectoryFileSystemNode("/");
+  let current: DirectoryFileSystemNode = root;
+  for (let command of outputLines) {
+    // console.log(command);
+    if (command === "$ cd /") {
+      current = root;
+      continue;
+    }
+    if (/^\d* .*/.test(command)) {
+      const [sizeString, fileName] = command.split(" ");
+      current.addFileChildNode(
+        new FileFileSystemNode(fileName, Number(sizeString), current)
+      );
+      continue;
+    }
+    if (/^\$ cd [a-z].?[a-z]*$/.test(command)) {
+      const [_$, _cd, dirName] = command.split(" ");
+
+      const dirNode = new DirectoryFileSystemNode(dirName, current);
+      current.addDirectoryChildNode(dirNode);
+      current = dirNode;
+      continue;
+    }
+    if (/^\$ cd \.\.$/.test(command)) {
+      current = current.parent!;
+      continue;
+    }
+  }
+  return root;
+}
+
+export function sumForDirectory(root: DirectoryFileSystemNode): {
+  name: string;
+  size: number;
+}[] {
+  const childrenSizes = root.directoryChildren.flatMap(sumForDirectory);
+  return [
+    {
+      name: root.name,
+      size:
+        root.fileChildren.reduce((total, node) => total + node.size, 0) +
+        childrenSizes.reduce((t, c) => t + c.size, 0),
+    },
+    ...childrenSizes,
+  ];
+}
+
+function visualize(root: DirectoryFileSystemNode, prefix: string): string[] {
+  let out: string[] = [];
+  out.push(prefix + "-" + root.name + "/");
+  root.fileChildren.forEach((n) => {
+    out.push(prefix + " -" + n.name + ":" + n.size);
+  });
+  root.directoryChildren.forEach((n) => {
+    const childData = visualize(n, prefix + " ");
+    out = [...out, ...childData];
+  });
+  return out;
+}
+
+export function part1(input: string): number {
+  const root = readToTree(input.split("\n"));
+  // console.log(visualize(root, "").join("\n"));
+  const sums = sumForDirectory(root);
+  const lessThanOneHundredThousand = sums.filter(
+    (x) => x.size < oneHundredThousand
+  );
+  console.log(sums.map((x) => `${x.name}:${x.size}`).join("\n"));
+  let answer = lessThanOneHundredThousand.reduce((t, c) => c.size + t, 0);
+  if (answer === 1630322) {
+    console.error("1630322 is Wrong");
+    return -1;
+  }
+  return answer;
+}
+
+export function part2(input: string): number {
+  return 0;
+}
