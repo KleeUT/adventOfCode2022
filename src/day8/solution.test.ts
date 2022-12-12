e715f158514e8709c3934b478fa56427c75fc841
Klee Thomas <klee.ut@gmail.com>
2022-12-12 22:25:11 +1100

    :sparkle: day 8 complete

--- src/day8/solution.test.ts
+++ src/day8/solution.test.ts
@@ -1,58 +1,16 @@
 import { describe, expect, it, test } from "vitest";
-
-enum Visibility {
-  visible = "visible",
-  hidden = "hidden",
-}
-
-type AssessedTree = {
-  height: number;
-  visible: Visibility;
-};
-
-type AssessedTreeLine = AssessedTree[];
-
-function assessTreeLine(treeLine: number[]): AssessedTreeLine {
-  const result = treeLine.reduce(
-    (assessed, current, index, array) => {
-      const previousTree = assessed.assessed.at(-1);
-      let visible = Visibility.visible;
-      if (
-        previousTree &&
-        (previousTree.height >= current || current <= assessed.highest)
-      ) {
-        console.log("setting to hidden", { current, previousTree });
-        visible = Visibility.hidden;
-      }
-
-      return {
-        highest: Math.max(assessed.highest, current),
-        assessed: [...assessed.assessed, { height: current, visible }],
-      };
-    },
-    { assessed: [], highest: 0 } as {
-      assessed: AssessedTreeLine;
-      highest: number;
-    }
-  );
-  return result.assessed;
-}
-
-function assessTreeLineBidirectional(treeLine: number[]): AssessedTreeLine {
-  const assessedForward = assessTreeLine(treeLine);
-  const assessedBackward = assessTreeLine(treeLine.reverse()).reverse();
-  return assessedForward.map((tree, index) => {
-    const otherTree = assessedBackward[index];
-
-    return {
-      ...tree,
-      visible:
-        tree.visible === Visibility.visible
-          ? Visibility.visible
-          : otherTree.visible,
-    };
-  });
-}
+import {
+  assessTreeLine,
+  assessTreeLineBidirectional,
+  columnsAsRows,
+  combineAssessments,
+  countVisibleTrees,
+  countVisibleTreesFromTree,
+  findTreeLines,
+  part1,
+  part2,
+  Visibility,
+} from "./solution";
 
 describe("day 8", () => {
   it("should find visible trees in one direction", () => {
@@ -83,5 +41,207 @@ describe("day 8", () => {
       { height: 4, visible: Visibility.visible },
       { height: 3, visible: Visibility.visible },
     ]);
+  });
+  it("should find visible trees in two directions 25512", () => {
+    const treeLine = [2, 5, 5, 1, 2];
+    const newRow = assessTreeLineBidirectional(treeLine);
+    expect(newRow).toEqual([
+      { height: 2, visible: Visibility.visible },
+      { height: 5, visible: Visibility.visible },
+      { height: 5, visible: Visibility.visible },
+      { height: 1, visible: Visibility.hidden },
+      { height: 2, visible: Visibility.visible },
+    ]);
+  });
+  it("should find visible trees in two directions 71349", () => {
+    const treeLine = [7, 1, 3, 4, 9];
+    const newRow = assessTreeLineBidirectional(treeLine);
+    expect(newRow).toEqual([
+      { height: 7, visible: Visibility.visible },
+      { height: 1, visible: Visibility.hidden },
+      { height: 3, visible: Visibility.hidden },
+      { height: 4, visible: Visibility.hidden },
+      { height: 9, visible: Visibility.visible },
+    ]);
+  });
+  test.each([
+    {
+      input: [3, 3, 5, 4, 9],
+      output: [
+        { height: 3, visible: Visibility.visible },
+        { height: 3, visible: Visibility.hidden },
+        { height: 5, visible: Visibility.visible },
+        { height: 4, visible: Visibility.hidden },
+        { height: 9, visible: Visibility.visible },
+      ],
+    },
+  ])("$input finds correct rows", ({ input, output }) => {
+    const newRow = assessTreeLineBidirectional(input);
+    expect(newRow).toEqual(output);
+  });
+  it("can invert an array", () => {
+    expect(
+      columnsAsRows([
+        ["1", "2"],
+        ["3", "4"],
+      ])
+    ).toEqual([
+      ["1", "3"],
+      ["2", "4"],
+    ]);
+  });
+  it("double invert an array returns to normal", () => {
+    const input = [
+      ["1", "2", "a", "b"],
+      ["3", "4", "c", "d"],
+      ["5", "6", "e", "f"],
+      ["7", "8", "g", "h"],
+    ];
+    expect(columnsAsRows(columnsAsRows(input))).toEqual([
+      ["1", "2", "a", "b"],
+      ["3", "4", "c", "d"],
+      ["5", "6", "e", "f"],
+      ["7", "8", "g", "h"],
+    ]);
+  });
+  it("can combine two assessments", () => {
+    const one = [
+      [
+        { height: 1, visible: Visibility.visible },
+        { height: 3, visible: Visibility.visible },
+        { height: 2, visible: Visibility.visible },
+        { height: 4, visible: Visibility.visible },
+        { height: 3, visible: Visibility.visible },
+      ],
+      [
+        { height: 1, visible: Visibility.visible },
+        { height: 2, visible: Visibility.hidden },
+        { height: 2, visible: Visibility.hidden },
+        { height: 4, visible: Visibility.hidden },
+        { height: 3, visible: Visibility.visible },
+      ],
+    ];
+    const two = [
+      [
+        { height: 1, visible: Visibility.visible },
+        { height: 3, visible: Visibility.visible },
+        { height: 2, visible: Visibility.hidden },
+        { height: 4, visible: Visibility.visible },
+        { height: 3, visible: Visibility.visible },
+      ],
+      [
+        { height: 1, visible: Visibility.hidden },
+        { height: 2, visible: Visibility.hidden },
+        { height: 2, visible: Visibility.visible },
+        { height: 4, visible: Visibility.hidden },
+        { height: 3, visible: Visibility.hidden },
+      ],
+    ];
+    const expected = [
+      [
+        { height: 1, visible: Visibility.visible },
+        { height: 3, visible: Visibility.visible },
+        { height: 2, visible: Visibility.visible },
+        { height: 4, visible: Visibility.visible },
+        { height: 3, visible: Visibility.visible },
+      ],
+      [
+        { height: 1, visible: Visibility.visible },
+        { height: 2, visible: Visibility.hidden },
+        { height: 2, visible: Visibility.visible },
+        { height: 4, visible: Visibility.hidden },
+        { height: 3, visible: Visibility.visible },
+      ],
+    ];
+
+    expect(combineAssessments(one, two)).toEqual(expected);
+  });
+  describe("Part 1", () => {
+    test("with sample", () => {
+      const lines = ["30373", "25512", "65332", "33549", "35390"];
+      expect(part1(lines.join("\n"))).toEqual(21);
+    });
+  });
+
+  describe("Viewfinder", () => {
+    test("find relevant trees", () => {
+      const input = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+      ];
+
+      const output = {
+        treesNorth: [2],
+        treesSouth: [8],
+        treesEast: [6],
+        treesWest: [4],
+      };
+
+      expect(findTreeLines({ row: 1, column: 1 }, input)).toEqual(output);
+    });
+
+    test.each([
+      { line: [1, 2, 3, 4, 5], tree: 5, expectedCount: 5 },
+      { line: [1, 2, 5, 4, 5], tree: 5, expectedCount: 3 },
+      { line: [], tree: 5, expectedCount: 0 },
+      { line: [5], tree: 5, expectedCount: 1 },
+      { line: [9], tree: 5, expectedCount: 1 },
+      { line: [3], tree: 5, expectedCount: 1 },
+      { line: [3, 6], tree: 5, expectedCount: 2 },
+    ])(
+      "Count the visible trees: $line | tree: $tree",
+      ({ tree, line, expectedCount }) => {
+        expect(countVisibleTrees(tree, line)).toEqual(expectedCount);
+      }
+    );
+
+    describe("count trees visible", () => {
+      test("count 1", () => {
+        const map = [
+          [1, 1, 2],
+          [1, 2, 2],
+          [2, 1, 2],
+          [1, 1, 2],
+        ];
+
+        expect(countVisibleTreesFromTree({ row: 2, column: 1 }, map)).toEqual(
+          1
+        );
+      });
+      test("count 2", () => {
+        const map = [
+          [1, 1, 2],
+          [1, 2, 2],
+          [2, 1, 2],
+          [1, 1, 2],
+        ];
+
+        expect(countVisibleTreesFromTree({ row: 1, column: 1 }, map)).toEqual(
+          2
+        );
+      });
+
+      test("count consecutive 0", () => {
+        const map = [
+          [1, 1, 3, 1, 2],
+          [1, 2, 1, 2, 2],
+          [6, 0, 2, 9, 9],
+          [1, 1, 1, 1, 2],
+          [1, 2, 5, 2, 2],
+        ];
+
+        expect(countVisibleTreesFromTree({ row: 2, column: 2 }, map)).toEqual(
+          8
+        );
+      });
+    });
+  });
+
+  describe("Part 2", () => {
+    test("with sample", () => {
+      const lines = ["30373", "25512", "65332", "33549", "35390"];
+      expect(part2(lines.join("\n"))).toEqual(8);
+    });
   });
 });
--- src/day8/solution.ts
+++ src/day8/solution.ts
@@ -1,7 +1,196 @@
+import { i } from "vitest/dist/index-9f5bc072";
+
+export enum Visibility {
+  visible = "visible",
+  hidden = "hidden",
+}
+
+export type AssessedTree = {
+  height: number;
+  visible: Visibility;
+};
+
+export type AssessedTreeLine = AssessedTree[];
+
+export function assessTreeLine(treeLine: number[]): AssessedTreeLine {
+  const result = treeLine.reduce(
+    (assessed, current) => {
+      const previousTree = assessed.assessed.at(-1);
+      let visible = Visibility.visible;
+      if (
+        previousTree &&
+        (previousTree.height > current || current <= assessed.highest)
+      ) {
+        visible = Visibility.hidden;
+      }
+
+      return {
+        highest: Math.max(assessed.highest, current),
+        assessed: [...assessed.assessed, { height: current, visible }],
+      };
+    },
+    { assessed: [], highest: 0 } as {
+      assessed: AssessedTreeLine;
+      highest: number;
+    }
+  );
+  return result.assessed;
+}
+
+function combineLines(
+  one: AssessedTreeLine,
+  two: AssessedTreeLine
+): AssessedTreeLine {
+  return one.map((tree, index) => {
+    const otherTree = two[index];
+
+    return {
+      ...tree,
+      visible:
+        tree.visible === Visibility.visible
+          ? Visibility.visible
+          : otherTree.visible,
+    };
+  });
+}
+export function assessTreeLineBidirectional(
+  treeLine: number[]
+): AssessedTreeLine {
+  const assessedForward = assessTreeLine(treeLine);
+  const assessedBackward = assessTreeLine(treeLine.reverse()).reverse();
+  return combineLines(assessedForward, assessedBackward);
+}
+
+export function columnsAsRows<T>(twoD: T[][]): T[][] {
+  const out: T[][] = [];
+  twoD.forEach((row, rowIndex) => {
+    row.forEach((column, columnIndex) => {
+      if (!out[columnIndex]) {
+        out[columnIndex] = [];
+      }
+      out[columnIndex][rowIndex] = column;
+    });
+  });
+  return out;
+}
+
+export function combineAssessments(
+  one: AssessedTreeLine[],
+  two: AssessedTreeLine[]
+): AssessedTreeLine[] {
+  return one.map((line, index) => {
+    return combineLines(line, two[index]);
+  });
+}
+
+function parseToMap(input: string): Map {
+  return input.split("\n").map((line) => line.split("").map(Number));
+}
+
 export function part1(input: string): number {
-  return 0;
+  const lines = parseToMap(input);
+  const lines2 = parseToMap(input);
+  const leftToRight = lines.map(assessTreeLineBidirectional);
+  const topToBottom = columnsAsRows(
+    columnsAsRows(lines2).map(assessTreeLineBidirectional)
+  );
+  const combined = combineAssessments(leftToRight, topToBottom);
+  return combined.flatMap((r) =>
+    r.filter((x) => x.visible === Visibility.visible)
+  ).length;
+}
+
+export type TreeLocation = {
+  row: number;
+  column: number;
+};
+export type Map = number[][];
+export type Tree = {
+  height: number;
+  location: TreeLocation;
+};
+export function findTreeLines(
+  tree: TreeLocation,
+  map: Map
+): {
+  treesNorth: number[];
+  treesSouth: number[];
+  treesEast: number[];
+  treesWest: number[];
+} {
+  const treesWest = map.at(tree.row)?.slice(0, tree.column)!;
+  const treesEast = map.at(tree.row)?.slice(tree.column + 1)!;
+  const { treesNorth, treesSouth } = map.reduce(
+    (p, c, i) => {
+      if (i < tree.row) {
+        p.treesNorth.push(c[tree.column]);
+      }
+      if (i > tree.row) {
+        p.treesSouth.push(c[tree.column]);
+      }
+      return p;
+    },
+    {
+      treesNorth: [] as number[],
+      treesSouth: [] as number[],
+    }
+  );
+  const result = {
+    treesNorth: treesNorth.reverse(),
+    treesSouth,
+    treesEast,
+    treesWest: treesWest.reverse(),
+  };
+  return result;
+}
+
+export function countVisibleTrees(tree: number, treeLine: number[]): number {
+  let i = 0;
+  for (; i < treeLine.length; i++) {
+    if (treeLine[i] >= tree) {
+      return i + 1;
+    }
+  }
+  return treeLine.length;
+}
+
+export function countVisibleTreesFromTree(
+  treeLocation: TreeLocation,
+  map: Map
+): number {
+  const { treesEast, treesNorth, treesSouth, treesWest } = findTreeLines(
+    treeLocation,
+    map
+  );
+  const north = countVisibleTrees(
+    map[treeLocation.row][treeLocation.column],
+    treesNorth
+  );
+  const east = countVisibleTrees(
+    map[treeLocation.row][treeLocation.column],
+    treesEast
+  );
+  const south = countVisibleTrees(
+    map[treeLocation.row][treeLocation.column],
+    treesSouth
+  );
+  const west = countVisibleTrees(
+    map[treeLocation.row][treeLocation.column],
+    treesWest
+  );
+  return north * south * east * west;
 }
 
 export function part2(input: string): number {
-  return 0;
+  let max = 0;
+  const map = parseToMap(input);
+  for (let r = 0; r < map.length; r++) {
+    for (let c = 0; c < map[r].length; c++) {
+      const visible = countVisibleTreesFromTree({ row: r, column: c }, map);
+      if (visible > max) {
+        max = visible;
+      }
+    }
+  }
+  return max;
 }
